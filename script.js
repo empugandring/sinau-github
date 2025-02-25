203aa1260e826f115766d57384e360e452d21282
zamroni <empugandring.sgs@gmail.com>
2025-02-25 14:29:28 +0700

    ubah js di branch coba

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log("ini console dari user lain");
+console.log("ini console dari user lain")
+
+let umur=20;
+if(umur<18){
+    console.log("tidak boleh masuk")
+}else{
+    console.log("ok")
+}
